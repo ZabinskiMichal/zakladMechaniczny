a0d1764b70a8bd7237df96e5a9186ad99e649700
Mateusz Zygmunt <matzyg1@gmail.com>
2022-11-29 20:14:51 +0100

    Przeorganizowanie struktury plików

--- zakladMechaniczny/src/main/java/com/example/zakladmechanicznyspringboot/controller/IndexController.java
+++ zakladMechaniczny/src/main/java/com/example/zakladmechanicznyspringboot/controller/IndexController.java
@@ -1,15 +1,10 @@
 package com.example.zakladmechanicznyspringboot.controller;
 
-import com.example.zakladmechanicznyspringboot.model.User;
-import com.example.zakladmechanicznyspringboot.model.UserLogging;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Objects;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class IndexController {
@@ -20,49 +15,49 @@ public class IndexController {
     UserRepository userRepository;
 
 
-    @GetMapping("/")
-    public String welcomePage() {
-
-//            return "rejestracja";
-        return "stronaPowitalna";
-    }
-
-
-    //    action zalogowany jest w htmlu stronaPowitalna
-    @PostMapping("/zalogowany")
-    public String logowanie(@ModelAttribute UserLogging userLogging, Model model) {
-        //tutaj trzeba będzie zaimplenetować logowanie do systemu
-
-        if (Objects.isNull(userRepository.loginUser(userLogging))) {
-            System.out.println("mamy nula");
-            System.out.println("nie ma takiego usera w wybranej tabeli");
-            return "stronaPowitalna";
-        } else {
-
-            //jezeli nie zwroci nulla to tworzymy obiekt
-            User zalogowany = new User(userRepository.loginUser(userLogging));
-
-            if (Objects.equals(userLogging.getType(), "Wlasciciel")) {
-
-                //tutaj bedziemu tworzyc poszczegolne pbiekty
-                return "widokHomeWlasciciela";
-
-            } else if (Objects.equals(userLogging.getType(), "Kierownik")) {
-                System.out.println("Loguje sie Kierownik");
-
-                System.out.println(zalogowany);
-                return "widokHomeKierownika";
-
-
-            } else if (Objects.equals(userLogging.getType(), "Pracownik")) {
-                System.out.println(zalogowany);
-                return "widokHomePracownika";
-            }
-
-        }
-
-        return "welcome";
-    }
+//    @GetMapping("/")
+//    public String welcomePage() {
+//
+////            return "rejestracja";
+//        return "strona_startowa";
+//    }
+//
+//
+//    //    action zalogowany jest w htmlu stronaPowitalna
+//    @PostMapping("/zalogowany")
+//    public String logowanie(@ModelAttribute UserLogging userLogging, Model model) {
+//        //tutaj trzeba będzie zaimplenetować logowanie do systemu
+//
+//        if (Objects.isNull(userRepository.loginUser(userLogging))) {
+//            System.out.println("mamy nula");
+//            System.out.println("nie ma takiego usera w wybranej tabeli");
+//            return "stronaPowitalna";
+//        } else {
+//
+//            //jezeli nie zwroci nulla to tworzymy obiekt
+//            User zalogowany = new User(userRepository.loginUser(userLogging));
+//
+//            if (Objects.equals(userLogging.getType(), "Wlasciciel")) {
+//
+//                //tutaj bedziemu tworzyc poszczegolne pbiekty
+//                return "widokHomeWlasciciela";
+//
+//            } else if (Objects.equals(userLogging.getType(), "Kierownik")) {
+//                System.out.println("Loguje sie Kierownik");
+//
+//                System.out.println(zalogowany);
+//                return "widokHomeKierownika";
+//
+//
+//            } else if (Objects.equals(userLogging.getType(), "Pracownik")) {
+//                System.out.println(zalogowany);
+//                return "widokHomePracownika";
+//            }
+//
+//        }
+//
+//        return "welcome";
+//    }
 
 
 //        return "zalogowanyUser";
@@ -70,30 +65,31 @@ public class IndexController {
 //    }
 
 
-//    @GetMapping("/")
-//    public String registerPage(){
-//        return "rejestracja";
-//    }
+    @GetMapping("/")
+    public String registerPage(){
+        return "strona_startowa";
+    }
 
 
     //teraz metoda typu posta ktora pobierze input
     //sprawdzamy który przycisk user
-//    @PostMapping("/wyborLoginRegister")
-//    public String logowanie(@RequestBody String wybor){
-//        System.out.println(wybor);
-//
-//        if(wybor.equals("upvote=Logowanie")){
-//            System.out.println("uzytkownik ma konto");
-//            return "logowanie";
-//
-//        }else if(wybor.equals("upvote=Rejestracja")){
-//            System.out.println("rejestracja nowego uzytkownika");
-//            return "rejestracja";
-//        }
-//
-//        return null;
-//
-//    }
+    @PostMapping("/wyborLoginRegister")
+    public String logowanie(@RequestBody String wybor){
+        System.out.println(wybor);
+
+        if(wybor.equals("upvote=Logowanie")){
+            System.out.println("uzytkownik ma konto");
+            return "logowanie";
+            //tu w zależności od wyboru
+
+        }else if(wybor.equals("upvote=Rejestracja")){
+            System.out.println("rejestracja nowego uzytkownika");
+            return "rejestracja";
+        }
+
+        return null;
+
+    }
 
 
 //    @PostMapping("/rejestracja")
